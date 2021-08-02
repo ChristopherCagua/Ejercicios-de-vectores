7e4231d00e00eb584f3c7785eb41d58435bc3b0d
ChristopherCagua <caguachristopher@gmail.com>
2021-08-01 22:33:22 -0500

    Trabajo sobre vectores

---
+++ cmake-build-debug/CMakeFiles/Ejercicios_sobre_vectores.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicios_sobre_vectores.dir/main.cpp.obj"
+  "Ejercicios_sobre_vectores.exe"
+  "Ejercicios_sobre_vectores.exe.manifest"
+  "Ejercicios_sobre_vectores.pdb"
+  "libEjercicios_sobre_vectores.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicios_sobre_vectores.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
